f96fa7d1c32fe8cc05190d60171498fd43095a1c
Voste126 <steveaustine126@gmail.com>
2023-09-25 20:06:52 +0300

    my experince with web scrapping

--- lib/scraper.py
+++ lib/scraper.py
@@ -2,5 +2,10 @@ from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
+#just incase you get a 403 error on forbidden error
 headers = {'user-agent': 'my-app/0.0.1'}
+#we grab the HTML page we need to use
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+#sample part of the part we need to scrap
+print(doc.select('.heading-financier'))
